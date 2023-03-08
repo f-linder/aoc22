8d458671fa7c36b8aa07745042695bd0eebe43d8
Felix Linder <felix@pop-os.localdomain>
2023-03-08 19:14:47 +0100

    day 04

---
+++ 04.cpp
@@ -0,0 +1,71 @@
+#include <stdexcept>
+#include <string>
+#include <fmt/core.h>
+#include "parser.h"
+#include <string_view>
+#include <utility>
+#include <iostream>
+
+struct Assignment {
+    int begin, end;
+};
+
+class Cleanup {
+    std::vector<std::pair<Assignment, Assignment>> pairs;
+
+public:
+    Cleanup(std::string filename) {
+        auto lines = Parser::readLines(filename);
+        for (const auto & l : lines) {
+            if (l.empty()) continue;
+
+            // pair of assignments
+            const auto & p = Parser::readStrings(l, ',');
+            if (p.size() != 2) continue; 
+
+            // assignment of both elfs
+            const auto & a1 = Parser::readInts(p.at(0), '-');
+            const auto & a2 = Parser::readInts(p.at(1), '-');
+            if (a1.size() != 2 && a2.size() != 2) continue; 
+                 
+            pairs.push_back(std::make_pair(Assignment{a1.at(0), a1.at(1)}, Assignment{a2.at(0), a2.at(1)}));
+        }
+    }
+    
+    // checks whether first contains seconds
+    bool contains(Assignment first, Assignment second) {
+        return (first.begin <= second.begin && first.end >= second.end);
+    }
+
+    int numContainments() {
+        int sum{0};
+        for (const auto & [a1, a2] : pairs) {
+            if (contains(a1, a2) || contains(a2, a1)) sum++;
+        }
+        return sum;
+    }
+    
+    // first overlaps with second ()
+    bool overlap(Assignment first, Assignment second) {
+        return (first.end >= second.begin && first.begin && second.end >= first.begin);
+    }
+
+    int numOverlaps() {
+        int sum{0};
+        for (const auto & [a1, a2] : pairs) {
+            if (overlap(a1, a2))  sum++;
+        }
+        return sum;
+    }
+
+};
+
+int main() {
+    fmt::print("AOC-22 #04\n");
+   
+    Cleanup c = {"../data/04.txt"};
+    
+    fmt::print("number of containments: {}\n", c.numContainments());
+    fmt::print("number of overlaps: {}\n", c.numOverlaps());
+    return 0;
+}
--- parser.h
+++ parser.h
@@ -10,49 +10,48 @@
 class Parser {
 public:
     // open file "filename" and return vector of strings representing each line of file
-    static std::vector<std::string> readLines(std::string fileName);
+    static std::vector<std::string> readLines(std::string fileName) {
+        std::ifstream input{fileName};
+        if(!input.is_open()) {
+            throw std::invalid_argument(fmt::format("error opening file{}", fileName));
+        }
+        
+        std::vector<std::string> lines;
+        std::string line;
+
+        while(std::getline(input, line)) {
+            lines.emplace_back(line);
+        }
+
+        fmt::print("read {} lines from {}\n", lines.size(), fileName);
+        return lines;
+    }
+
     // parse delimiter seperated ints from given string
-    static std::vector<int> readInts(std::string filename, char deliminiter);
+    static std::vector<int> readInts(std::string string, char delimiter) {
+        std::vector<int> items;
+        std::string item;
+        std::stringstream ss(string);
 
+        while(std::getline(ss, item, delimiter)) {
+            if (item.empty()) continue;
+            items.push_back(std::stoi(item));
+        }
+
+        return items;
+    }
+
+    // parse delimiter seperated strings from given string
+    static std::vector<std::string> readStrings(std::string string, char delimiter) {
+        std::vector<std::string> items;
+        std::string item;
+        std::stringstream ss(string);
+
+        while(std::getline(ss, item, delimiter)) {
+            if (item.empty()) continue;
+            items.push_back(item);
+        }
+
+        return items;
+    }
 };
-
-
-
-std::vector<std::string> Parser::readLines(std::string fileName) {
-    std::ifstream input{fileName};
-    if(!input.is_open()) {
-        throw std::invalid_argument(fmt::format("error opening file{}", fileName));
-    }
-    
-    std::vector<std::string> lines;
-    std::string line;
-
-    while(std::getline(input, line)) {
-        lines.emplace_back(line);
-    }
-
-    fmt::print("read {} lines from {}\n", lines.size(), fileName);
-    return lines;
-}
-
-std::vector<int> readInts(const std::string& string, char delimiter) {
-    std::vector<int> items;
-    std::string item;
-    std::stringstream ss(string);
-
-    while(std::getline(ss, item, delimiter)) {
-        if (item.empty()) continue;
-        items.push_back(std::stoi(item));
-    }
-
-    return items;
-}
-
-
-
-
-
-
-
-
-
